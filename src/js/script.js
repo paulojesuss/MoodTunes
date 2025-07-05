94b35342a7654667b350d25bddd34df67c5dc981
Paulo Eduardo <pe911586@gmail.com>
2025-07-05 13:07:48 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+ScrollReveal().reveal('.titulo', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.card', {
+    origin: 'left',
+    duration: 1000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.mb-3', {
+    origin: 'left',
+    duration: 1000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.btn', {
+    origin: 'left',
+    duration: 1000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.img-fluid', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.sobre', {
+    origin: 'right',
+    duration: 2000,
+    distance: '20%'
+});
