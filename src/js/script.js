5aebf90c806a1a6a447fbc538613df13cd0e1609
Paulo Eduardo <pe911586@gmail.com>
2025-07-05 13:42:32 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,37 @@
-ScrollReveal().reveal('.titulo', {
-    origin: 'left',
-    duration: 2000,
-    distance: '20%'
-});
-
-ScrollReveal().reveal('.card', {
-    origin: 'left',
-    duration: 1000,
-    distance: '20%'
-});
-
-ScrollReveal().reveal('.mb-3', {
-    origin: 'left',
-    duration: 1000,
-    distance: '20%'
-});
-
-ScrollReveal().reveal('.btn', {
-    origin: 'left',
-    duration: 1000,
-    distance: '20%'
-});
-
-ScrollReveal().reveal('.img-fluid', {
-    origin: 'left',
-    duration: 2000,
-    distance: '20%'
-});
-
-ScrollReveal().reveal('.sobre', {
-    origin: 'right',
-    duration: 2000,
-    distance: '20%'
+$(document).ready(function () {
+    ScrollReveal().reveal('.titulo', {
+        origin: 'left',
+        duration: 2500,
+        distance: '20%'
+    });
+    
+    ScrollReveal().reveal('.card', {
+        origin: 'left',
+        duration: 1500,
+        distance: '20%'
+    });
+    
+    ScrollReveal().reveal('.mb-3', {
+        origin: 'left',
+        duration: 1500,
+        distance: '20%'
+    });
+    
+    ScrollReveal().reveal('.btn', {
+        origin: 'left',
+        duration: 1500,
+        distance: '20%'
+    });
+    
+    ScrollReveal().reveal('.img-fluid', {
+        origin: 'left',
+        duration: 2500,
+        distance: '20%'
+    });
+    
+    ScrollReveal().reveal('.sobre', {
+        origin: 'right',
+        duration: 2500,
+        distance: '20%'
+    });
 });
